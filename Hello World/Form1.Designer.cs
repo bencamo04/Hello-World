485c6c7eb650642c3b7692efe5e55e152980a043
Benjamin Cameron <RC0041043@student.riversidecollege.ac.uk>
2020-12-14 14:58:54 +0000

    Added text box

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.txtBxName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -37,26 +38,37 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 59);
             this.button1.TabIndex = 0;
-            this.button1.Text = "hello";
+            this.button1.Text = "Hello";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtBxName
+            // 
+            this.txtBxName.Location = new System.Drawing.Point(322, 266);
+            this.txtBxName.Name = "txtBxName";
+            this.txtBxName.Size = new System.Drawing.Size(132, 20);
+            this.txtBxName.TabIndex = 1;
+            this.txtBxName.Text = "Welcome to Ben\'s program";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBxName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBxName;
     }
 }
 
